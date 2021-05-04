7234a90220e69c8c2104630eb437d59576ee2c82
CarrieLiang24 <cliang@bayviewglen.ca>
2021-05-04 11:15:10 -0400

    Added runWall command word for train station

--- src/textAdventure/Game.java
+++ src/textAdventure/Game.java
@@ -24,7 +24,7 @@ public class Game {
     try {
       player = new Character(new Inventory(100));
       initRooms("src\\textAdventure\\data\\rooms.json");
-      currentRoom = roomMap.get("Bedroom");
+      currentRoom = roomMap.get("TrainStation");
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -117,6 +117,8 @@ public class Game {
         return true; // signal that we want to quit
     } else if (commandWord.equals("eat")) {
       System.out.println("Do you really think you should be eating at a time like this?");
+    } else if (commandWord.equals("run")) {
+      runWall(command);
     }
     return false;
   }
@@ -158,7 +160,25 @@ public class Game {
       System.out.println(currentRoom.longDescription());
     }
   }
+  private void runWall(Command command) {
+    if (!command.hasSecondWord()) {
+      // if there is no second word, we don't know where to go...
+      System.out.println("Go where?");
+      return;
+    }
 
+    String direction = command.getSecondWord();
+
+    // Try to leave current room.
+    Room nextRoom = currentRoom.nextRoom(direction);
+
+    if (nextRoom == null)
+      System.out.println("There is no door!");
+    else {
+      currentRoom = nextRoom;
+      System.out.println(currentRoom.longDescription());
+    }
+  }
   private void takeItem(Item item) {
     if (currentRoom.getItems().contains(item)) {
       currentRoom.getInventory().removeItem(item);

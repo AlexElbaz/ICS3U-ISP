74e40caa4c481d355af0fbba22b91ccb7e92f252
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-05-06 09:11:07 -0400

    Added command words take and drop, did some modifications to the boardTrain() and runWall() functions.

--- src/textAdventure/Game.java
+++ src/textAdventure/Game.java
@@ -169,41 +169,44 @@ public class Game {
   private void runWall(Command command) {
     if (!command.hasSecondWord()) {
       // if there is no second word, we don't know where to go...
-      System.out.println("Go where?");
+      System.out.println("Run where?");
       return;
     }
+    if (command.getSecondWord().equals("at the wall")) {
+      Room nextRoom = currentRoom.nextRoom("east");
+        // direction of wall exit from player
 
-    String direction = command.getSecondWord();
-
-    // Try to leave current room.
-    Room nextRoom = currentRoom.nextRoom(direction);
-
-    if (nextRoom == null)
-      System.out.println("There is no door!");
-    else {
-      currentRoom = nextRoom;
-      System.out.println(currentRoom.longDescription());
+      if (nextRoom == null)
+        System.out.println("There's nowhere to go there.");
+      else {
+        currentRoom = nextRoom;
+        System.out.println(currentRoom.longDescription());
+      }
+    } else {
+      System.out.println("There's nowhere to go there.");
     }
   }
   private void boardTrain(Command command) {
     if (!command.hasSecondWord()) {
       // if there is no second word, we don't know where to go...
-      System.out.println("Go where?");
+      System.out.println("Board what?");
       return;
     }
+    if (command.getSecondWord().equals("train")) {
+      Room nextRoom = currentRoom.nextRoom("east");
+        // direction of train exit from player
 
-    String direction = command.getSecondWord();
-
-    // Try to leave current room.
-    Room nextRoom = currentRoom.nextRoom(direction);
-
-    if (nextRoom == null)
-      System.out.println("There is no door!");
-    else {
-      currentRoom = nextRoom;
-      System.out.println(currentRoom.longDescription());
+      if (nextRoom == null)
+        System.out.println("You can't board that.");
+      else {
+        currentRoom = nextRoom;
+        System.out.println(currentRoom.longDescription());
+      }
+    } else {
+      System.out.println("You can't board that.");
     }
   }
+
   private void takeItem(String item) {
     boolean itemExists = false;
       for (int i = 0; i < currentRoom.getItems().size(); i++) {

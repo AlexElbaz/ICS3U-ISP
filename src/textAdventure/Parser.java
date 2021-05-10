98c6d02671994ce561a5ff850364e1dd841241bb
Alex Elbaz <aelbaz@bayviewglen.ca>
2021-05-10 11:40:34 -0400

    Made updates to the parser class. With the updates the Command and CommadWords classes are completely obselete and so I have deleted them. Note that the run at the wall command seems to not work properly, though it does not throw an error, so that should be checked out

--- src/textAdventure/Parser.java
+++ src/textAdventure/Parser.java
@@ -1,14 +1,14 @@
 package textAdventure;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Parser {
-  private CommandWords commands; // holds all valid command words
+  private ArrayList<String> validCommands = new ArrayList<String>(Arrays.asList("go", "quit", "help", "eat", "run", "board", "take", "drop"));
   private Scanner in;
 
   public Parser() {
-    //commands = new CommandWords();
     in = new Scanner(System.in);
   }
 
@@ -34,34 +34,12 @@ public class Parser {
     }
 
     return words;
-    
-    /*
-    String inputLine = "";
-    String[] words;
-
-    System.out.print("> "); // print prompt
-
-    inputLine = in.nextLine();
-
-    words = inputLine.split(" ");
-
-    String word1 = words[0];
-    String word2 = null;
-    if (words.length > 1)
-      word2 = words[1];
-
-    if (commands.isCommand(word1))
-      return new Command(word1, word2);
-    else
-      return new Command(null, word2);
-
-    */
   }
 
   /**
    * Print out a list of valid command words.
   */
   public void showCommands() {
-    commands.showAll();
+    System.out.println(validCommands);
   }
 }

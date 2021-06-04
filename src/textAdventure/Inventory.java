d0ce1da93f2d2faa9a48be20c077c939d61d35a2
Trevor Cheung <tcheung@bayviewglen.ca>
2021-06-04 10:29:49 -0400

    Updated comments for Inventory, finished comments for Character

--- src/textAdventure/Inventory.java
+++ src/textAdventure/Inventory.java
@@ -8,7 +8,7 @@ public class Inventory {
   private long currentWeight;
 
   /**
-   * initializes the arraylist of items in the inventory, the max weight that the inventory can hold, and the current weight (the weight of all the items) of the inventory
+   * initializes the new inventory's arraylist of items in the inventory, the max weight that the inventory can hold, and the current weight (the weight of all the items) of the inventory
    * @param maxWeight the max weight that the inventory can hold
    */
   public Inventory(long maxWeight) {
@@ -55,8 +55,8 @@ public class Inventory {
    * @return true if there is enough room in the inventory to hold the item, false if there is not enough room
    */
   public boolean addItem(Item item) {
-    if (item.getWeight() + currentWeight <= maxWeight) {
-      currentWeight += item.getWeight();
+    if (item.getWeight() + currentWeight <= maxWeight) { // checks if the specified item's weight and the weight of all the other items in the inventory is less than or equal to the max weight of the inventory that calls this method
+      currentWeight += item.getWeight(); // updates the current weight of the inventory that calls it by adding the specified item's weight
       return items.add(item);
     } else {
       System.out.println("There is no room to add the item.");
@@ -70,20 +70,20 @@ public class Inventory {
    * @return true if the item exists in the inventory that calls it, false if the item does not exist in the inventory that calls it
    */
   public boolean removeItem(Item item) {
-    currentWeight -= item.getWeight();
+    currentWeight -= item.getWeight(); // updates the current weight of the inventory that calls it by subtracting the specified item's weight
     return items.remove(item);
   }
 
   /**
-   * returns a string that shows what is in the inventory that calls it
-   * @return a string that shows what is in the inventory that calls it
+   * returns a string that shows the items, the max weight, and the remaining space left in the inventory that calls it
+   * @return a string that shows the items, the max weight, and the remaining space left in the inventory that calls it
    */
   public String viewInventory() {
     String output;
-    if (items.size() != 0){
+    if (items.size() != 0){ // if there are items in the inventory that calls this method
       output = "You see: ";
       for (Item item : items) {
-        output += item.getName() + " ";
+        output += item.getName() + " "; // creates a new string and adds the names of all the items that are in the inventory that calls this method
       }
   } else {
     output = "There is nothing in your inventory.";
